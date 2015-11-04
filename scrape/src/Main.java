240883fbad9c5e9f077b0ae08b6e569c8766d371
Vinesh Kannan <vingkan@gmail.com>
2015-11-03 21:14:10 -0600

    Added page limits

--- scrape/src/Main.java
+++ scrape/src/Main.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.util.PDFTextStripper;
 //import org.apache.pdfbox.searchengine.lucene.LucenePDFDocument;
@@ -12,8 +15,29 @@ public class Main {
 		File file = new File("elastandards1.pdf");
 		PDDocument doc = PDDocument.load(file);
 		System.out.println(doc);
+		stripper.setStartPage(30);
+		stripper.setEndPage(30);
+		//stripper.writeText(doc, System.out);
 		String text = stripper.getText(doc);
-		System.out.println(text);
+		//System.out.println(text);
+		List<String> standards = new ArrayList<String>();
+		int size = text.length();
+		Boolean inLine = true;
+		String line = "";
+		for(int c = 0; c < size; c++){
+			char current = text.charAt(c);
+			System.out.println("-------------: " + current);
+			if(current == '\n' && inLine){
+				standards.add(line);
+				//inLine = false;
+			}
+			else if(inLine){
+				line += current;
+			}
+		}
+		for(String standard : standards){
+			//System.out.println("---" + standard);
+		}
 	}
 
 }
