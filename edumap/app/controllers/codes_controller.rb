6c326b9a0653549b333b06c545e44432caa93b22
Patrick Sier <pjsier@gmail.com>
2016-01-13 10:52:29 -0600

    very rough start at using filterrific

---
+++ edumap/app/controllers/codes_controller.rb
@@ -0,0 +1,38 @@
+class CodesController < ApplicationController
+
+  def index
+    @filterrific = initialize_filterrific(
+      Code,
+      params[:filterrific],
+      select_options: {
+        sorted_by: Code.options_for_sorted_by,
+        with_standard_id: Standard.options_for_select
+      },
+      persistence_id: 'shared_key',
+      default_filter_params: {},
+      available_filters: [],
+    ) or return
+
+    # Get an ActiveRecord::Relation for all students that match the filter settings.
+    # You can paginate with will_paginate or kaminari.
+    # NOTE: filterrific_find returns an ActiveRecord Relation that can be
+    # chained with other scopes to further narrow down the scope of the list,
+    # e.g., to apply permissions or to hard coded exclude certain types of records.
+    @codes = @filterrific.find.page(params[:page])
+
+    # Respond to html for initial page load and to js for AJAX filter updates.
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
+  # Recover from invalid param sets, e.g., when a filter refers to the
+  # database id of a record that doesn’t exist any more.
+  # In this case we reset filterrific and discard all filter params.
+  rescue ActiveRecord::RecordNotFound => e
+    # There is an issue with the persisted param_set. Reset it.
+    puts "Had to reset filterrific params: #{ e.message }"
+    redirect_to(reset_filterrific_url(format: :html)) and return
+  end
+
+end
--- edumap/config/routes.rb
+++ edumap/config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'lessons#index'
+  root 'codes#index'
 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  resources :lessons
+  resources :codes
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
