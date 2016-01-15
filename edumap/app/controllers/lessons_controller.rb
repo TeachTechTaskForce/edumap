02c004200f78feaa6f013bb1c99321cb4f364123
Patrick Sier <pjsier@gmail.com>
2016-01-15 17:07:27 -0600

    filterrific working for lessons, curricula

--- edumap/app/controllers/lessons_controller.rb
+++ edumap/app/controllers/lessons_controller.rb
@@ -1,8 +1,19 @@
 class LessonsController < ApplicationController
 
   def index
-    @lessons = Lesson.all
+    @filterrific = initialize_filterrific(
+      Lesson,
+      params[:filterrific],
+      :select_options => {
+        sorted_by: Lesson.options_for_sorted_by,
+        with_standard_id: Standard.options_for_select
+      }
+    ) or return
+    @lessons = @filterrific.find.page(params[:page])
 
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
-
 end
