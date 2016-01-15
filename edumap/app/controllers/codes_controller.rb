ab2e00bbc8902662bbc58a3460cb2f87833f440f
Patrick Sier <pjsier@gmail.com>
2016-01-15 09:37:57 -0600

    filterrific basically working, a few issues with data structures

--- edumap/app/controllers/codes_controller.rb
+++ edumap/app/controllers/codes_controller.rb
@@ -4,29 +4,16 @@ class CodesController < ApplicationController
     @filterrific = initialize_filterrific(
       Code,
       params[:filterrific],
-      select_options: {
+      :select_options => {
         sorted_by: Code.options_for_sorted_by,
         with_standard_id: Standard.options_for_select
-      },
-      persistence_id: 'shared_key',
-      default_filter_params: {},
-      available_filters: [],
+      }
     ) or return
-
     @codes = @filterrific.find.page(params[:page])
 
     respond_to do |format|
       format.html
       format.js
     end
-
-  # Recover from invalid param sets, e.g., when a filter refers to the
-  # database id of a record that doesn’t exist any more.
-  # In this case we reset filterrific and discard all filter params.
-  rescue ActiveRecord::RecordNotFound => e
-    # There is an issue with the persisted param_set. Reset it.
-    puts "Had to reset filterrific params: #{ e.message }"
-    redirect_to(reset_filterrific_url(format: :html)) and return
   end
-
 end
