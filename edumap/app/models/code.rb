75e3320db99373cf5ababecfc513a7ac17ede368
pjsier <pjsier@gmail.com>
2016-01-15 00:09:52 -0600

    continued filter edits

--- edumap/app/models/code.rb
+++ edumap/app/models/code.rb
@@ -1,18 +1,16 @@
 class Code < ActiveRecord::Base
-  belongs_to :standard
-  has_and_belongs_to_many :lessons
-
-  filterrific(
-  default_filter_params: { sorted_by: 'created_at_desc' },
-  available_filters: [
-    :sorted_by,
-    :search_query,
-    :with_standard_id,
-    :with_created_at_gte
-  ]
-  )
+  filterrific :default_filter_params => { :sorted_by => 'created_at_desc' },
+                :available_filters => %w[
+                  sorted_by
+                  search_query
+                  with_standard_id
+                  with_created_at_gte
+                ]
 
   self.per_page = 10
+
+  belongs_to :standard
+  has_and_belongs_to_many :lessons
 
   scope :sorted_by, lambda { |sort_option|
     # extract the sort direction from the param value.
@@ -20,6 +18,10 @@ class Code < ActiveRecord::Base
     case sort_option.to_s
     when /^created_at_/
       order("codes.created_at #{ direction }")
+    when /^name_/
+      order("LOWER(codes.identifier) #{ direction }")
+    when /^standard_abbreviation_/
+      order("LOWER(standards.abbreviation) #{ direction }").includes(:standard)
     else
       raise(ArgumentError, "Invalid sort option: #{ sort_option.inspect }")
     end
@@ -35,7 +37,10 @@ class Code < ActiveRecord::Base
 
   def self.options_for_sorted_by
     [
+      ['Identifier (a-z)', 'identifier_asc'],
       ['Creation date (newest first)', 'created_at_desc'],
+      ['Creation date (oldest first)', 'created_at_asc'],
+      ['Standard (a-z)', 'standard_asc']
     ]
   end
 end
