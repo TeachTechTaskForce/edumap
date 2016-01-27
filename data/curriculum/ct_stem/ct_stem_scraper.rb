ab2d2316b1acaa62e30227f0885b5d7c9fe7e057
Becca Nelson <beccanelson88@gmail.com>
2016-01-26 21:11:36 -0600

    Add ct-stem scraper & CSV

--- data/curriculum/ct_stem/ct_stem_scraper.rb
+++ data/curriculum/ct_stem/ct_stem_scraper.rb
@@ -4,7 +4,7 @@ require 'CSV'
 require 'open-uri'
 doc = Nokogiri::HTML(open("http://ct-stem.northwestern.edu/assess/lesson-plans/lesson-plans.html"))
 
-CSV.open("edumap/mappings/ct_stem.csv", "wb") do |csv|
+CSV.open("ct_stem.csv", "wb") do |csv|
   doc.css(".lesson-plan-table td a").each do |node|
     link = "http://http://ct-stem.northwestern.edu#{node['href']}"
     csv << [node.text, link]
