927e25914bf7a439f86f32effe3faae986c65a08
Vinesh Kannan <vingkan@gmail.com>
2016-01-26 21:23:26 -0600

    Added binary number pdfs

--- scrape/not_java/main.py
+++ scrape/not_java/main.py
@@ -1,2 +1,17 @@
 import os;
-os.system('pdf2txt.py -o outputfile.txt unplugged/01binarynumbers.pdf')
+from os import listdir;
+from os.path import isfile, join;
+
+
+BASE_PATH = 'pdf/'
+
+folders = [(BASE_PATH + folderPath + '/') for folderPath in listdir(BASE_PATH)];
+
+filesToScrape = [(folderPath + filename) for filename in listdir(folderPath) for folderPath in folders]
+
+
+for filename in filesToScrape:
+	print filename
+
+#for(filename in files):
+	#os.system('pdf2txt.py -o output/' + filename + '.txt ' + filename + '.pdf')
