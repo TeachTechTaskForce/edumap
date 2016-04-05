1e8ffe393954bed550cc1a0b2f0e0940a5eea99a
Kevin Yaroch <kjyaroch@gmail.com>
2016-04-04 19:02:41 -0500

    Lesson::search_query now searches code identifier and code description

--- edumap/app/models/lesson.rb
+++ edumap/app/models/lesson.rb
@@ -21,13 +21,13 @@ class Lesson < ActiveRecord::Base
     # most people use * for wildcard searches
     # MySQL uses % instead for reasons
     terms = query.downcase.split(/\s+/)
-                 .map{|e| e.gsub('*', '%')}
-                 .map{|e| "%#{e}%"}
-
     terms.inject(self) do |current_scope, term|
-      current_scope.where(
-          "LOWER(lessons.name) LIKE :term OR LOWER(lessons.lesson_url) LIKE :term",
-          term: term
+      current_scope.includes(:codes).references(:codes).where(
+          "LOWER(lessons.name) LIKE :wildcard_term " \
+          "OR LOWER(lessons.lesson_url) LIKE :wildcard_term " \
+          "OR LOWER(codes.description) LIKE :wildcard_term " \
+          "OR LOWER(codes.identifier) = :term",
+          term: term, wildcard_term: "%#{term.gsub('*', '%')}%"
       )
     end
   }
