82523e06e309339ab8e5236a4b39832e7225e56f
Vinesh Kannan <vingkan@gmail.com>
2016-04-20 07:14:31 +0000

    Hackish JS

---
+++ edumap/app/mailers/sessions_mailer.rb
@@ -0,0 +1,6 @@
+class SessionsMailer < ApplicationMailer
+  def lessons_email(email_address, lessons)
+    @lessons = lessons
+    mail(to: email_address, subject: "Edumap: Your Saved Lessons")
+  end
+end
