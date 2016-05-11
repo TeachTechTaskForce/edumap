82368d38818bc3ca60df75feeca09c62172a320c
Hillel Wayne <hwayne@gmail.com>
2016-05-10 20:22:02 -0500

    bootstrap scraper now standardized

--- data/curriculum/bootstrap/bootstrap_scraper.rb
+++ data/curriculum/bootstrap/bootstrap_scraper.rb
@@ -6,14 +6,42 @@ require 'open-uri'
 materials_bootstrap1_page = Nokogiri::HTML(open("http://www.bootstrapworld.org/materials/spring2016/courses/bs1/index.shtml"))
 materials_bootstrap2_page = Nokogiri::HTML(open("http://www.bootstrapworld.org/materials/spring2016/courses/bs2/index.shtml"))
 
-File.open("bootstrapworld.txt", "w") do |file|
-  materials_bootstrap1_page.xpath("//a[starts-with(text(), 'Unit')]/@href").each do |partial_link|
-    current_unit_page = Nokogiri::HTML(open("http://www.bootstrapworld.org/materials/spring2016/courses/bs1/#{partial_link}"))
-    current_page_title = current_unit_page.css(".BootstrapTitle").text
-    current_page_standards = current_unit_page.css(".LearningObjectivesListItemContents p span").map(&:text)
-    file.puts(current_page_title, current_page_standards)
+def header
+  ['url', 'standard_code', 'standard_desc', 'title', 'description', 'length']
+end
 
+class Page
+  attr_reader :link
+  def initialize(link)
+    @link = link
+    @html = Nokogiri::HTML(open(@link))
   end
-  file.close
 
+  def title
+    @html.css(".BootstrapTitle").text
+  end
+
+  def standards
+    @html.css(".LearningObjectivesListItemContents > p").map(&:text)
+  end
+
+  def description
+    @html.css("#overviewDescr p").text
+  end
+
+  def length
+    @html.css(".summary > span.BootstrapHeader").text.split(':').last
+  end
+end
+
+CSV.open("bootstrap_world.csv", "w") do |csv|
+  csv << header
+
+  materials_bootstrap1_page.xpath("//a[starts-with(text(), 'Unit')]/@href").each do |partial_link|
+    page = Page.new("http://www.bootstrapworld.org/materials/spring2016/courses/bs1/#{partial_link}")
+    page.standards.each do |standard|
+      standard_code, standard_desc = standard.split(':')
+      csv << [page.link, standard_code, standard_desc, page.title, page.description, page.length]
+    end
+  end
 end
