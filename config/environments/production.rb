55628458b829c331ff1a5f74334abeefdfec34ab
reginafcompton <reginafcompton@gmail.com>
2016-08-02 20:00:48 -0500

    Create smtp settings for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,20 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'edumap3.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "edumap3.herokuapp.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: SENDGRID_USERNAME,
+      password: SENDGRID_PASSWORD
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
---
+++ config/initializers/smtp_settings.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'gmail.com',
+    :authentication => :plain,
+    :user_name => SENDGRID_USERNAME,
+    :password => SENDGRID_PASSWORD
+}
